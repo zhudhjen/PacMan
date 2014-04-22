a7d81f8c8a77b8eb3dbd8de7cb784f5e1904db1c
Zhu Dinghan <zhudhjen@gmail.com>
2014-04-22 23:33:09 +0800

    complete AI framework

--- ai.c
+++ ai.c
@@ -1,16 +1,6 @@
 #include "ai.h"
 
-void aiConstruct (const classAI *this, double ai_speed, double player_speed)
-{
-    this->construct(this, ai_speed, player_speed);
-}
-
-int aiMove (const classAI *this, const int **map, const bool burst)
+int moveAI (const classAI *this, const int **map, const bool burst)
 {
     return this->move(this, map, burst);
-}
-
-void aiDesruct (const classAI *this)
-{
-    this->destruct(this);
 }
--- ai.h
+++ ai.h
@@ -1,18 +1,21 @@
+#ifndef _AI_H_
+#define _AI_H_
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct {
-    //construct the AI
-    void (*construct)(const classAI *this, double ai_speed, double player_speed);
+typedef int bool;
 
+typedef struct tagClassAI classAI;
+typedef struct tagClassAI {
     //move the monsters
     int (*move)(const classAI *this, const int **map, const bool burst);
-
-    //be notified when game over, destruct the AI
-    void (*destruct)(const classAI *this);
     
     double aiSpeed, playerSpeed;
 } classAI;
 
+int moveAI (const classAI *this, const int **map, const bool burst);
+
+#endif /*_AI_H_*/
