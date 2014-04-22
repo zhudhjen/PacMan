dd5e5613c398878b0923417e70d05e531323cf69
Zhu Dinghan <zhudhjen@gmail.com>
2014-04-22 23:35:35 +0800

    add AI:fool

---
+++ fool.h
@@ -0,0 +1,18 @@
+#ifndef _FOOL_H_
+#define _FOOL_H_
+
+#include "ai.h"
+
+typedef struct {
+    //inherit from ai
+    classAI ai;
+
+} classFool;
+
+extern void foolConstruct (classFool *this, const double ai_speed, const double player_speed);
+
+static int foolMove (const classAI *this, const int **map, const bool burst);
+
+extern void foolDestruct (const classAI *this);
+
+#endif /*_FOOL_H_*/
