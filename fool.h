7e8d1bbeda0d0c113623c5efad8a5fab95cfd912
Zhu Dinghan <zhudhjen@gmail.com>
2014-06-01 03:47:29 +0800

    fool completed

--- fool.h
+++ fool.h
@@ -5,9 +5,6 @@
 
 extern void foolConstruct (classAI *this, double ai_speed, double player_speed, int index);
 
-static int foolMove (classAI *this, int map[MAP_HEIGHT][MAP_WIDTH], coord ghostPos[], int ghostDir[], double pacPosX, double pacPosY, int burst);
+static int foolMove (classAI *this, int map[MAP_HEIGHT][MAP_WIDTH], coord *ghostPos, int *ghostDir, double pacPosX, double pacPosY, int burst);
 
-extern void foolDestruct (classAI *this);
-
-static void foolDfs (int map[MAP_HEIGHT][MAP_WIDTH], coord position);
 #endif /*_FOOL_H_*/
