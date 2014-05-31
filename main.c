7041cbb61e2479df87fec63f24a6053b49655c25
王同和 <Hetong583@qq.com>
2014-06-01 01:18:45 +0800

    ?

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include "main.h"
 
+struct Global {
+  double playerSpeed = 0.5;
+  int score = 0;
+  int life = 2;
+} global;
+
+struct Pacman {
+  int screenX, screenY;
+  int mapX, mapY;
+  int dir;
+  int operation;
+} pacman;
+
+struct Ghost {
+  int screenX, screenY;
+  int mapX, mapY;
+  int dir;
+  classAI ai;
+  int burst;
+} ghost[4];
+
 int map[32][28] = {
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
   {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 0},
   {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
   {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
   {0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
@@ -25,7 +46,7 @@ int map[32][28] = {
   {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
   {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
   {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+  {0, 3, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 3, 0},
   {0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
   {0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0},
   {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0},
@@ -35,25 +56,137 @@ int map[32][28] = {
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
+int welcomePage() {
+  int mode = 0;
+  drawWelcomePage(mode);
+  while (1) {
+    if (bioskey(1))
+      key = bioskey(0);
+    else 
+      key = 0;
+    switch (key) {
+      case VK_UP:
+          mode--;
+          if (mode < 0)
+            mode = 2;
+          break;
+      case VK_DOWN:
+          mode = (mode + 1) % 3;
+          break;
+      case VK_ENTER:
+          return mode;
+    }
+    drawWelcomePage(mode);
+  }
+}
+
 void gameInitial() {
   int i;
   initMap(map);
-  classPerson pacman, ghost[4];
   pacman.mapX = 14;
   pacman.mapY = 17;  
   pacman.screenX = 14 * UNIT + UNIT / 2;
   pacman.screenY = 17 * UNIT + UNIT / 2;
-  drawPacman(pacman.screenX, pacman.screenY);
-  for (i = 0; i < 5; i++) {
+  pacman.dir = LEFT;
+  drawPacman(pacman.screenX, pacman.screenY, pacman.dir);
+  for (i = 0; i < 4; i++) {
     ghost[i].mapX = 12 + i * 2;
     ghost[i].mapY = 16;
     ghost[i].screenX = (12 + i * 2) * UNIT + UNIT / 2;
     ghost[i].screenY = 16 * UNIT + UNIT / 2;
-    drawGhost(ghost[i].screenX, ghost[i].screenY);
+    ghost[i].dir = UP;
+    drawGhost(i, ghost[i].screenX, ghost[i].screenY, ghost[i].dir);
+  }
+}
+
+void handleUp() {
+  if (map[p.mapX])
+}
+void handleDown();
+void handleLeft();
+void handleRight();
+
+void keyPress() {
+  int key,
+      x = pacman.mapX;
+      y = pacman.mapY;
+  if (bioskey(1))
+    key = bioskey(0);
+  else 
+    key = 0;
+  switch (key) {
+    case VK_LEFT:
+        pacman.operation = LEFT;
+        break;
+    case VK_RIGHT:
+        pacman.operation = RIGHT;
+        break;
+    case VK_UP:
+        pacman.operation = UP;
+        break;
+    case VK_DOWN:
+        pacman.operation = DOWN;
+        break;
+  }
+}
+
+void pacmanRound() {
+  clearPerson(p.mapX, p.mapY, map);
+  switch (p.dir) {
+    case UP:
+        handleUp(p);
+        break;
+    case DOWN:
+        handleDown(p);
+        break;
+    case LEFT:
+        handleLeft(p);
+        break;
+    case RIGHT:
+        handleRight(p);
+        break;
+  }
+}
+
+void process() {
+  static int timeCounter = 0,
+             actionFlag = 0;
+
+  if ((inportb(0x3da)&0x08) != 0 && actionFlag == 0 && (timeCounter % 2) == 0) {
+    round(pacman);
+    actionFlag = 1;
+  }
+  else if ((inportb(0x3da)&0x08) == 0 && actionFlag == 1) {
+    actionFlag = 0;
+    timeCounter++;
   }
 }
 
 int main() {
+  int mode, i;
+  mode = welcomePage();
   gameInitial();
-	
-} 
+
+  /* initialize ai
+  */
+  if (mode == 0) {
+    classFool ai[4];
+    for (i = 0; i < 4; i++)
+      foolConstruct(ai + i, global.aiSpeed, global.playerSpeed, i);
+  }
+  else if (mode == 1) {
+    classNormal ai[4];
+    for (i = 0; i < 4; i++)
+      normalConstruct(ai + i, global.aiSpeed, global.playerSpeed, i);
+  }
+  else if (mode == 2) {
+    classExpert ai[4];
+    for (i = 0; i < 4; i++)
+      expertConstruct(ai + i, global.aiSpeed, global.playerSpeed, i);
+  }
+
+	while(1) {
+    keyPress();
+    process();           
+  }
+}
--- main.h
+++ main.h
@@ -2,16 +2,28 @@
 #include "fool.c"
 #include "ai.h"
 #include "ai.c"
+#include "dos.h"
 
 #define MAP_WIDTH 24
 #define MAP_HEIGHT 28
+
 #define UP 0
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
 
-typedef struct {
-	int screenX, screenY;
-	int mapX, mapY;
-	int moveDir;
-} classPerson;
+#define VK_UP 0x4800
+#define VK_DOWN 0x5000
+#define VK_LEFT 0x4b00
+#define VK_RIGHT 0x4d00
+#define VK_ENTER 0x1c0d
+
+void welcomePage();
+void gameInitial();
+void keyPress();
+void process();
+void pacmanRound();
+void handleUp();
+void handleDown();
+void handleLeft();
+void handleRight();
